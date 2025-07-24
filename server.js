e8052327fe64b814af62397c6bb95ee7dd10627e
Rafael <rafaelcasadio2@gmail.com>
2025-07-24 02:17:34 -0300

    feat: add server

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const agentesRoutes = require("./routes/agentesRoutes");
+const casosRoutes = require("./routes/casosRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/agentes", agentesRoutes);
+app.use("/casos", casosRoutes);
+
+app.listen(PORT, () => {
+  console.log(
+    `Servidor do Departamento de Polícia rodando em localhost:${PORT}`
+  );
+});
